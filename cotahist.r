401f692f4f39c746340cfd98f7feb0b8b52f2102
fdiang <fdiang@gmail.com>
2015-10-20 14:20:00 -0200

    Update cotahist.r

--- cotahist.r
+++ cotahist.r
@@ -11,8 +11,8 @@ for (cotahist in cotahist_list)
     {
     assets_full <- read_fwf(file, 
                      fwf_widths(c(2, 8, 2, 12, 3, 12, 10, 3, 4, 13, 13, 13, 13, 13, 13, 13, 5, 18, 18, 13, 1, 8, 7, 13, 12, 3)), 
-                     col_names = c("TIPREG", "DATE", "CODBDI", "CODNEG", "TPMERC", "NOMRES", "ESPECI", "PRAZOT", "MODREF", "PREABE", "PREMAX", "PREMIN", "PREMED", "PREULT", "PREOFC", "PREOFV", "TOTNEG", "QUATOT", "VOLTOT", "PREEXE", "INDOPC", "DATVEN", "FATCOT", "PTOEXE", "CODISI", "DISMES"))
-    
+                     col_names = c("TIPREG", "DATE", "CODBDI", "CODNEG", "TPMERC", "NOMRES", "ESPECI", "PRAZOT", "MODREF", "PREABE", "PREMAX", "PREMIN", "PREMED", "PREULT", "PREOFC", "PREOFV", "TOTNEG", "QUATOT", "VOLTOT", "PREEXE", "INDOPC", "DATVEN", "FATCOT", "PTOEXE", "CODISI", "DISMES")
+                     progress=interactive())
   }
   
   # if the merged dataset assets_full does exist, append to it
@@ -21,8 +21,7 @@ for (cotahist in cotahist_list)
     temp_dataset <- read_fwf(cotahist, 
                      fwf_widths(c(2, 8, 2, 12, 3, 12, 10, 3, 4, 13, 13, 13, 13, 13, 13, 13, 5, 18, 18, 13, 1, 8, 7, 13, 12, 3)), 
                      col_names = c("TIPREG", "DATE", "CODBDI", "CODNEG", "TPMERC", "NOMRES", "ESPECI", "PRAZOT", "MODREF", "PREABE", "PREMAX", "PREMIN", "PREMED", "PREULT", "PREOFC", "PREOFV", "TOTNEG", "QUATOT", "VOLTOT", "PREEXE", "INDOPC", "DATVEN", "FATCOT", "PTOEXE", "CODISI", "DISMES")
-    )
-    
+                     progress=interactive())
     assets_full <- rbind(assets_full, temp_dataset)
     rm(temp_dataset)
     }
