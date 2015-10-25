ac93e81a218da1a9ea2747cb8db80194ccf55a33
fdiang <fdiang@gmail.com>
2015-10-25 12:26:01 -0200

    Update cotahist.r
    Tested and identified that read_fwf does not work well with a list as file parameter.

--- cotahist.r
+++ cotahist.r
@@ -3,28 +3,27 @@ cotahist_list <- list.files()
 assets_full <- data.frame()
 assets <- data.frame()
 
-for (cotahist in cotahist_list) 
-  {
-  rprintf("Processing file: %s", cotahist)
+for (cotahist in cotahist_list){
   # if the merged dataset assets_full doesn"t exist, create it
   if (!exists("assets_full"))
-    {
-    assets_full <- read_fwf(file, 
-                     fwf_widths(c(2, 8, 2, 12, 3, 12, 10, 3, 4, 13, 13, 13, 13, 13, 13, 13, 5, 18, 18, 13, 1, 8, 7, 13, 12, 3)), 
-                     col_names = c("TIPREG", "DATE", "CODBDI", "CODNEG", "TPMERC", "NOMRES", "ESPECI", "PRAZOT", "MODREF", "PREABE", "PREMAX", "PREMIN", "PREMED", "PREULT", "PREOFC", "PREOFV", "TOTNEG", "QUATOT", "VOLTOT", "PREEXE", "INDOPC", "DATVEN", "FATCOT", "PTOEXE", "CODISI", "DISMES")
-                     progress=interactive())
+  {
+    assets_full <- read_fwf(cotahist, 
+                            fwf_widths(c(2, 8, 2, 12, 3, 12, 10, 3, 4, 13, 13, 13, 13, 13, 13, 13, 5, 18, 18, 13, 1, 8, 7, 13, 12, 3), 
+                            col_names = c("TIPREG", "DATE", "CODBDI", "CODNEG", "TPMERC", "NOMRES", "ESPECI", "PRAZOT", "MODREF", "PREABE", "PREMAX", "PREMIN", "PREMED", "PREULT", "PREOFC", "PREOFV", "TOTNEG", "QUATOT", "VOLTOT", "PREEXE", "INDOPC", "DATVEN", "FATCOT", "PTOEXE", "CODISI", "DISMES")),
+                            progress=interactive())
+    
   }
   
-  # if the merged dataset assets_full does exist, append to it
+  #if the merged dataset assets_full does exist, append to it
   if (exists("assets_full"))
-    {
+  {
     temp_dataset <- read_fwf(cotahist, 
-                     fwf_widths(c(2, 8, 2, 12, 3, 12, 10, 3, 4, 13, 13, 13, 13, 13, 13, 13, 5, 18, 18, 13, 1, 8, 7, 13, 12, 3)), 
-                     col_names = c("TIPREG", "DATE", "CODBDI", "CODNEG", "TPMERC", "NOMRES", "ESPECI", "PRAZOT", "MODREF", "PREABE", "PREMAX", "PREMIN", "PREMED", "PREULT", "PREOFC", "PREOFV", "TOTNEG", "QUATOT", "VOLTOT", "PREEXE", "INDOPC", "DATVEN", "FATCOT", "PTOEXE", "CODISI", "DISMES")
-                     progress=interactive())
+                             fwf_widths(c(2, 8, 2, 12, 3, 12, 10, 3, 4, 13, 13, 13, 13, 13, 13, 13, 5, 18, 18, 13, 1, 8, 7, 13, 12, 3), 
+                             col_names = c("TIPREG", "DATE", "CODBDI", "CODNEG", "TPMERC", "NOMRES", "ESPECI", "PRAZOT", "MODREF", "PREABE", "PREMAX", "PREMIN", "PREMED", "PREULT", "PREOFC", "PREOFV", "TOTNEG", "QUATOT", "VOLTOT", "PREEXE", "INDOPC", "DATVEN", "FATCOT", "PTOEXE", "CODISI", "DISMES")),
+                            progress=interactive())
     assets_full <- rbind(assets_full, temp_dataset)
     rm(temp_dataset)
-    }
+  }
 }
 
 # slice and dice observations to what means something
